e75cf140248a59cf505f619d60861121dc6d3f14
donatowolfisberg <donato@wolfisberg.dev>
2023-05-01 13:23:15 +0200

    working drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react'
+import {DragEvent, ReactEventHandler, SyntheticEvent, useRef, useState} from 'react'
 import './App.css'
 import useSWR from "swr";
 
@@ -28,21 +28,57 @@ const images = [
 ]
 
 function App() {
-    const [index, setIndex] = useState(0)
+    const [index, setIndex] = useState(0);
+    const [testDiv, setTestDiv] = useState({
+        top: '100px',
+        left: '100px',
+        width: '100px',
+        height: '200px',
+    });
     const [imageInfo, setImageInfo] = useState<undefined | { offsetHeight: number, offsetWith: number }>(undefined)
+    const [dragInfo, setDragInfo] = useState<undefined | { startX: number, startY: number }>(undefined)
 
     const next = () => setIndex(current => (current + 1) % images.length)
     const currentImage = () => images[index];
 
     const imageRef = useRef<HTMLImageElement>(null);
 
-    const onDragStart = (e: React.DragEvent<HTMLImageElement>) => {
-        e.preventDefault();
-        console.log(e)
-        return false;
+    const onDragStart = (e: DragEvent<HTMLImageElement>) => {
+        // e.preventDefault();
+        let newVar = {
+            startX: e.clientX,
+            startY: e.clientY
+        };
+        setDragInfo(newVar);
+        console.log('drag start', newVar)
+        // return false;
+    }
+    const onDragEnd = (e: DragEvent<HTMLImageElement>) => {
+        // e.preventDefault();
+        const newVar = {
+            top: `${dragInfo!.startY}px`,
+            left: `${dragInfo!.startX}px`,
+            height: `${e.clientY - dragInfo!.startY}px`,
+            width: `${e.clientX - dragInfo!.startX}px`,
+        };
+        setTestDiv(newVar);
+        console.log('drag end', newVar)
+        // return false;
+    }
+    const onDrag = (e: DragEvent<HTMLImageElement>) => {
+        // e.preventDefault();
+        const newVar = {
+            top: `${dragInfo!.startY}px`,
+            left: `${dragInfo!.startX}px`,
+            height: `${e.clientY - dragInfo!.startY}px`,
+            width: `${e.clientX - dragInfo!.startX}px`,
+        };
+        setTestDiv(newVar);
+        // console.log('drag', newVar)
+        // return false;
     }
 
-    const onImgLoad: React.ReactEventHandler<HTMLImageElement> = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    const onImgLoad: ReactEventHandler<HTMLImageElement> = (e: SyntheticEvent<HTMLImageElement>) => {
         console.log('onLoad', e);
         setImageInfo({
             offsetHeight: e.currentTarget.offsetHeight,
@@ -55,6 +91,8 @@ function App() {
             <img
                 ref={imageRef}
                 onDragStart={onDragStart}
+                onDragEnd={onDragEnd}
+                onDrag={onDrag}
                 onClick={next}
                 onLoad={onImgLoad}
                 className={'large-img'}
@@ -62,12 +100,13 @@ function App() {
             ></img>
             {imageRef.current?.src}
             {JSON.stringify(imageInfo)}
+            {JSON.stringify(dragInfo)}
             <div style={{
                 border: '1px solid black',
-                top: '100px',
-                left: '100px',
-                width: '100px',
-                height: '200px',
+                top: testDiv.top,
+                left: testDiv.left,
+                width: testDiv.width,
+                height: testDiv.height,
                 position: 'absolute'
             }}>
 
