7672bb69c72754397cd55d877e58eb82440c0bc5
donatowolfisberg <donato@wolfisberg.dev>
2023-05-02 10:31:04 +0200

    memoize links list

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useMemo, useState} from 'react'
 import './Viewer.css'
 import {toRelativeRectangle} from "./models/graphic.ts";
 import {RectangleDiv} from "./RectangleDiv.tsx";
@@ -20,30 +20,32 @@ function ViewerLoaded(props: { collection: Images }) {
     const [imageId, setImageId] = useState<string>('Basis');
     const [imageRectangle, imageRef] = useImageRectangle();
 
-    const image = () => {
+    const image = useMemo(() => {
         const image = props.collection[imageId];
         if (!image) throw new Error(`Image with id ${imageId} not found`);
         return image;
-    };
+    }, [props.collection, imageId]);
+
+    const links = useMemo(() => image.links.map((link, index) => {
+            return (
+                <RectangleDiv
+                    onClick={() => setImageId(link.targetId)}
+                    clickable={true}
+                    key={index}
+                    rectangle={toRelativeRectangle(imageRectangle, link.rectangle)}
+                ></RectangleDiv>
+            );
+        }), [image, imageRectangle]);
 
     return (
         <div className="paintArea">
             <img ref={imageRef}
                  className="image"
-                 src={image().src}
-                 alt={image().title}
+                 src={image.src}
+                 alt={image.title}
                  draggable={false}
             />
-            {image().links.map((link, index) => {
-                return (
-                    <RectangleDiv
-                        onClick={() => setImageId(link.targetId)}
-                        clickable={true}
-                        key={index}
-                        rectangle={toRelativeRectangle(imageRectangle, link.rectangle)}
-                    ></RectangleDiv>
-                );
-            })}
+            {links}
         </div>
     );
 }
