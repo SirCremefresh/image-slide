d6beeb4fe39e20cc5fb308897b02a0ab2414090f
donatowolfisberg <donato@wolfisberg.dev>
2023-05-11 13:49:15 +0200

    open dropdown on click

--- src/components/LinkEditModal.tsx
+++ src/components/LinkEditModal.tsx
@@ -1,9 +1,39 @@
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Image } from "@common/models/collection.ts";
 import { Combobox } from "@headlessui/react";
 import { classNames } from "../util/class-names.ts";
 import { assertNotNullOrUndefined } from "@common/util/assert-util.ts";
+
+function SearchListItem(props: {
+  selected: boolean;
+  image: Image;
+  active: boolean;
+}) {
+  return (
+    <>
+      <span
+        className={classNames(
+          "block truncate",
+          props.selected && "font-semibold"
+        )}
+      >
+        {props.image.title}
+      </span>
+
+      {props.selected && (
+        <span
+          className={classNames(
+            "absolute inset-y-0 right-0 flex items-center pr-4",
+            props.active ? "text-white" : "text-indigo-600"
+          )}
+        >
+          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+        </span>
+      )}
+    </>
+  );
+}
 
 function ImageAutocomplete(props: {
   images: Image[];
@@ -11,6 +41,11 @@ function ImageAutocomplete(props: {
   selectedImage: Image | null;
 }) {
   const [query, setQuery] = useState("");
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    buttonRef.current?.click();
+  }, [buttonRef]);
 
   const filteredImages =
     query === ""
@@ -32,55 +67,41 @@ function ImageAutocomplete(props: {
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
+          autoComplete="off"
           displayValue={(image: Image) => image?.title}
         />
-        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+        <Combobox.Button
+          ref={buttonRef}
+          className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
+        >
           <ChevronUpDownIcon
             className="h-5 w-5 text-gray-400"
             aria-hidden="true"
           />
         </Combobox.Button>
 
-        {filteredImages.length > 0 && (
-          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {filteredImages.map((person) => (
-              <Combobox.Option
-                key={person.imageId}
-                value={person}
-                className={({ active }) =>
-                  classNames(
-                    "relative cursor-default select-none py-2 pl-3 pr-9",
-                    active ? "bg-indigo-600 text-white" : "text-gray-900"
-                  )
-                }
-              >
-                {({ active, selected }) => (
-                  <>
-                    <span
-                      className={classNames(
-                        "block truncate",
-                        selected && "font-semibold"
-                      )}
-                    >
-                      {person.title}
-                    </span>
-
-                    {selected && (
-                      <span
-                        className={classNames(
-                          "absolute inset-y-0 right-0 flex items-center pr-4",
-                          active ? "text-white" : "text-indigo-600"
-                        )}
-                      >
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                    )}
-                  </>
-                )}
-              </Combobox.Option>
-            ))}
-          </Combobox.Options>
-        )}
+        <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          {filteredImages.map((image) => (
+            <Combobox.Option
+              key={image.imageId}
+              value={image}
+              className={({ active }) =>
+                classNames(
+                  "relative cursor-default select-none py-2 pl-3 pr-9",
+                  active ? "bg-indigo-600 text-white" : "text-gray-900"
+                )
+              }
+            >
+              {({ active, selected }) => (
+                <SearchListItem
+                  selected={selected}
+                  image={image}
+                  active={active}
+                />
+              )}
+            </Combobox.Option>
+          ))}
+        </Combobox.Options>
       </div>
     </Combobox>
   );
