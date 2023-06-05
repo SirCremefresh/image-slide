57258ace4a42e68040606388b604cb7342acb997
donatowolfisberg <donato@wolfisberg.dev>
2023-06-05 11:14:55 +0200

    add test to github ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
-// noinspection JSUnusedGlobalSymbols
+// eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
