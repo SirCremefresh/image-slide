af81e1cd2bb113a5bc2e4d0127d64aaa3a1700d7
donatowolfisberg <donato@wolfisberg.dev>
2023-06-06 18:23:27 +0200

    make plugin more configurable

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,57 @@
 // <reference types="vite" />
 
 import * as fs from "node:fs/promises";
+import * as path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { NormalizedOutputOptions, OutputBundle, Plugin } from "rollup";
 
-function earlyHints(): Plugin {
-  return {
-    name: "transform-file",
+type FileEnding = "css" | "js";
+type FileType = "style" | "script";
 
-    async writeBundle(src: NormalizedOutputOptions, a: OutputBundle) {
-      const files = Object.keys(a);
-      const editorFile = files.find((file) => file.includes("Editor-"));
-      const cssFile = files.find((file) => file.includes(".css"));
-      const fileName = `${src.dir}/_headers`;
-      const content = `
-/edit/*
-  Link: </${editorFile}>; rel=preload; as=script
-/*
-  Link: </${cssFile}>; rel=preload; as=style
-            `;
-      await fs.writeFile(fileName, content);
-      console.log(src.dir);
-      console.log("generateBundle", Object.keys(a));
+function fileEndingToType(fileEnding: FileEnding): FileType {
+  switch (fileEnding) {
+    case "css":
+      return "style";
+    case "js":
+      return "script";
+  }
+}
+
+function earlyHints(
+  options: { type: FileEnding; name: string; path: string }[]
+): Plugin {
+  return {
+    name: "early-hints",
+
+    async writeBundle(
+      outputOptions: NormalizedOutputOptions,
+      bundle: OutputBundle
+    ) {
+      const files = Object.keys(bundle);
+      let out = "";
+      for (const option of options) {
+        const fileName = files.find(
+          (file) =>
+            file.startsWith(path.join("assets", option.name)) &&
+            file.endsWith(option.type)
+        );
+        if (!fileName) {
+          throw new Error(
+            `File not found. name: ${option.name}, type: ${
+              option.type
+            }, availableFiles: ${JSON.stringify(files)}`
+          );
+        }
+        out += `${
+          option.path
+        }\n  Link: </${fileName}>; rel=preload; as=${fileEndingToType(
+          option.type
+        )}\n`;
+      }
+      const headersFileName = path.join(outputOptions.dir, "_headers");
+      await fs.writeFile(headersFileName, out);
     },
   };
 }
@@ -31,5 +59,20 @@ function earlyHints(): Plugin {
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), earlyHints()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    earlyHints([
+      {
+        name: "Editor",
+        type: "js",
+        path: "/edit/*",
+      },
+      {
+        name: "index",
+        type: "css",
+        path: "/*",
+      },
+    ]),
+  ],
 });
