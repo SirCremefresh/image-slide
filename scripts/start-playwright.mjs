a1efb71904b23cb13a8b7b77373383a822a5b0d3
donatowolfisberg <donato@wolfisberg.dev>
2023-06-14 10:52:59 +0200

    setting up playwright

---
+++ scripts/start-playwright.mjs
@@ -0,0 +1,68 @@
+import {exec as execCb, spawn} from 'child_process';
+import http from 'http';
+
+// Promise wrapper for exec
+const exec = (command) => {
+    return new Promise((resolve, reject) => {
+        execCb(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve({stdout, stderr});
+        });
+    });
+};
+
+const sleep = (ms) => new Promise(res => setTimeout(res, ms));
+
+// Function to execute a command
+const executeCommand = (command, args = [], stdoutAction, stderrAction) => {
+    const process = spawn(command, args);
+
+    process.stdout.on('data', stdoutAction);
+    process.stderr.on('data', stderrAction);
+
+    return process;
+};
+
+// Function to check if a site is reachable
+const waitForServer = (url) => new Promise((resolve, reject) => {
+    const checkConnection = () => {
+        http.get(url, (res) => {
+            if (res.statusCode === 200) resolve();
+            else setTimeout(() => {
+                console.log("Waiting for server to be reachable...")
+                checkConnection();
+            }, 300);
+        }).on('error', () => setTimeout(checkConnection, 300));
+    };
+
+    checkConnection();
+});
+
+// Execute the first command
+const pagesDevProcess = executeCommand(
+    'npm',
+    ['run', 'pages:dev'],
+    (data) => console.log(`pages:dev stdout: ${data}`),
+    (data) => console.error(`pages:dev stderr: ${data}`)
+);
+await sleep(1000)
+
+// Wait for the server to be reachable
+console.log('Waiting for server to be reachable...');
+await waitForServer('http://localhost:8788');
+console.log('Server is reachable.');
+
+// Execute the second command
+try {
+    console.log('Running playwright test');
+    const {stdout, stderr} = await exec('npx playwright test');
+    console.log(`playwright test stdout: ${stdout}`);
+    console.error(`playwright test stderr: ${stderr}`);
+} catch (error) {
+    console.error(`playwright test exec error: ${error}`);
+}
+
+pagesDevProcess.kill();
